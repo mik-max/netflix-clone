acc601c2a624de0a3d2d8ac4f4a937fe62dd5d28
Michael Chinye <michaelchinyechinye2018@gmail.com>
2021-12-20 14:39:24 +0100

    Built Footer.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
             <Route path = "/" element = {<LandingPage />} />
             <Route path = "/home" element={<Home />} /> 
           </Routes>
+          <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import './Footer.css';
+function Footer() {
+     return (
+          <>
+              <footer className = 'footer'>
+                   <p>Questions? Call 07087509689</p>
+                   <div className = 'footer-cols'>
+                        <ul>
+                             <li>
+                                  <a hrref = '#'>FAQ</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Investor Relations</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Corporate Information</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Netflix originals</a>
+                             </li>
+                        </ul>
+                        <ul>
+                             <li>
+                                  <a hrref = '#'>Help Center</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Jobs</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Terms Of Use</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Contact Us</a>
+                             </li>
+                        </ul>
+                        <ul>
+                             <li>
+                                  <a hrref = '#'>Account</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Redeem Gift Cards</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Privacy</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Speed Test</a>
+                             </li>
+                        </ul>
+                        <ul>
+                             <li>
+                                  <a hrref = '#'>Media Center</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Buy Gift Cards</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Cookie Preferences</a>
+                             </li>
+                             <li>
+                                  <a hrref = '#'>Legal Notices</a>
+                             </li>
+                        </ul>
+                   </div>
+              </footer> 
+          </>
+     )
+}
+
+export default Footer
